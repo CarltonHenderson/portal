fa31c8122cc164f68e803e6b7b9d71afe43912b8
Carlton Henderson <henderson.carlton@gmail.com>
2022-10-12 17:19:40 -0400

    better config not depending on sh

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import notecomm
 
-this_device = notecomm.card.Transaction({"req": "hub.get"})["sn"]
+this_devUID = notecomm.card.Transaction({"req": "hub.get"})["device"]
 orange_devUID = "dev:5c02723120f3"
 blue_devUID = "dev:94deb822f2a6"
 
-target_device = blue_devUID if this_device == "orange" else orange_devUID
+target_device = blue_devUID if this_devUID == blue_devUID else orange_devUID
 
 debug=True
 if debug:
