dcd8244e8bb6ab1b397fae604dd24eb2a8b3ad32
Carlton Henderson <henderson.carlton@gmail.com>
2022-10-12 17:29:10 -0400

    note delete true

--- notecomm.py
+++ notecomm.py
@@ -56,6 +56,7 @@ def get_from_notehub(imageName):
     req = {'req': 'note.get'}
     req['file'] = 'image.qi'
     req['sync'] = True
+    req['delete'] = True
 
     res = card.Transaction(req)
     if not 'image' in res:
